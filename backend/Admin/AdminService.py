e9a57d1575c4362ac527abe594134a169e500886
D Krishna Chaitanya <krishnachaitanyadevunoori@gmail.com>
2024-06-15 22:33:24 +0530

    add Admin service

---
+++ backend/Admin/AdminService.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, jsonify
+from database import mongo
+from bson.objectid import ObjectId
+
+admin_service = Blueprint('admin_service', __name__)
+
+@admin_service.route('/isadmin', methods=['POST'])
+def is_admin():
+    data = request.get_json()
+    admin = mongo.db.admins.find_one({'username':data['username'],'password': data['password']})
+    if admin:
+        return jsonify({'message': 'success'})
+    return jsonify({'message': 'Admin not found'})
+
+@admin_service.route('/admin', methods=['POST'])
+def create_admin():
+    data = request.get_json()
+    mongo.db.admins.insert_one(data)
+    return jsonify({'message': 'Admin created successfully'})
